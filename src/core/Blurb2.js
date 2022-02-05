f7398f55b4b1edf8d855c906fd93ff08ecdb1388
winutta <willinutter@gmail.com>
2022-02-04 21:54:05 -0800

    simple squareText

--- src/core/Blurb2.js
+++ src/core/Blurb2.js
@@ -1,39 +1,38 @@
 import {Text} from 'troika-three-text'
 import * as THREE from 'three'
 
-function BlurbBackground(type){
-	var geometry;
-	if(type == "popout"){
-		geometry = new THREE.PlaneGeometry( 1, 1 );
-	} else if(type == "button"){
-		geometry = new THREE.CircleGeometry( .25,32 );
+export class SquareText extends THREE.Group {
+	constructor(text, position) {
+		super();
+		this.position.copy(new THREE.Vector3(...position));
+
+		this.Text = new Text();
+		this.Background = this.generateMesh();
+
+		this.setText(text);
+		this.onTextLoad();
 	}
-	var material = new THREE.MeshBasicMaterial({color: "black"});
-	return new THREE.Mesh(geometry,material);
-}
 
-function Blurb(options){
-	this.Empty = new THREE.Group();
-	this.Empty.name = options.name;
-	this.Text = new Text();
-	this.Background = BlurbBackground(options.type);
-	this.Background.name = options.name;
-	this.Background.kind = options.type;
-	this.Background.Blurb = this;
+	generateMesh() {
+		var geometry = new THREE.PlaneGeometry(1, 1);
+		var material = new THREE.MeshBasicMaterial({ color: "black" });
+		return new THREE.Mesh(geometry, material);
+	}
 
+	sizeBackground() {
+			var { min, max } = this.Text.geometry.boundingBox;
+			var heightText = max.y - min.y;
+			var widthText = max.x - min.x;
+			this.Background.scale.set(widthText + 2 * this.Text.textInset, heightText + 2 * this.Text.textInset, 1.);
+	}
 
-	this.Empty.add(this.Background);
-	this.Empty.add(this.Text);
-
-	this.Empty.position.copy(options.offset);
-
-	this.setText = function(){
-		this.Text.text = options.text;
+	setText(text) {
+		this.Text.text = text;
 		this.Text.maxWidth = 4.;
 		this.Text.fontSize = 0.2;
 		this.Text.lineHeight = 1.5;
 		this.Text.textAlign = "center";
-		this.Text.position.set(0.,0.,0.01);
+		this.Text.position.set(0., 0., 0.01);
 		this.Text.anchorX = "center";
 		this.Text.anchorY = "50%";
 		this.Text.font = "DancingScript-Regular.ttf"
@@ -41,63 +40,11 @@ function Blurb(options){
 		this.Text.textInset = 0.1;
 	}
 
-	this.resizeRectangle = function(){
+	onTextLoad(){
 		this.Text.sync(() => {
-			var {min,max} = this.Text.geometry.boundingBox;
-			var heightText = max.y - min.y;
-			var widthText = max.x - min.x;
-
-			this.Background.scale.set(widthText + 2*this.Text.textInset,heightText + 2*this.Text.textInset,1.);
-			
+			this.sizeBackground();
+			this.add(this.Background);
+			this.add(this.Text);
 		});
 	}
-
-	this.setText();
-	if(options.type == "popout"){
-		this.resizeRectangle();
-	}
 }
-
-
-function blurbSystem(options){
-	this.system = new THREE.Group();
-	this.system.position.copy(options.position);
-
-	this.popoutOptions = {
-			text: options.popoutText,
-			offset: options.offset,
-			scene: options.scene,
-			type: "popout",
-			name: options.name,
-		};
-
-	this.buttonOptions = {
-		text: options.buttonText,
-		offset: new THREE.Vector3(0,0,0),
-		scene: options.scene,
-		type: "button",
-		name: options.name,
-	}
-
-	this.popout = new Blurb(this.popoutOptions);
-	this.popout.system = this;
-	this.button = new Blurb(this.buttonOptions);
-	this.button.system = this;
-
-	this.popout.Empty.scale.set(0,0,0);
-
-	this.system.add(this.popout.Empty);
-	this.system.add(this.button.Empty);
-
-	options.scene.add(this.system);
-
-	this.faceCamera = function(offsetVector){
-		var popoutTarget = this.popout.Empty.getWorldPosition(new THREE.Vector3()).add(offsetVector);
-		this.popout.Empty.lookAt(popoutTarget);
-
-		var buttonTarget = this.button.Empty.getWorldPosition(new THREE.Vector3()).add(offsetVector);
-		this.button.Empty.lookAt(buttonTarget);
-	}
-}
-
-export {blurbSystem};
--- src/script2.js
+++ src/script2.js
@@ -1,9 +1,9 @@
 import './style.css'
 import * as THREE from 'three'
-import {OrbitControls} from 'three/examples/jsm/controls/OrbitControls.js'
-import {Text} from 'troika-three-text'
 
 import {setup} from "./setup.js"
+
+import { SquareText} from "./core/Blurb2.js"
 
 
 function main() {
@@ -12,7 +12,18 @@ function main() {
 
 var {scene, camera, renderer} = setup();
 
+// ADD NEW SQUARE TEXT
 
+var sq2 = new SquareText("Goodbye",[2,0,0]);
+scene.add(sq2);
+
+var sq2 = new SquareText("Goodbye",[0,0,0]);
+scene.add(sq2);
+var sq2 = new SquareText("Goodbye",[2,2,0]);
+scene.add(sq2);
+
+var sq2 = new SquareText("Goodbye",[0,1,0]);
+scene.add(sq2);
 
 
 // RENDER LOOP
